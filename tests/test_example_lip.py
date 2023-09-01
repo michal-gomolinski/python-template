63259dacd7bbf5e319a09e04e3df7e3a1ac180a3
michal-gomolinski <michalgomol@gmail.com>
2023-09-01 18:28:26 +0200

    chore: run pre-commit

--- tests/test_example_lip.py
+++ tests/test_example_lip.py
@@ -1,9 +1,11 @@
 from template.example_lib import calculate_square, hello_world
 
+
 def test_hello_world():
     assert hello_world("world") == "hello world"
     assert hello_world("") == "Please introduce yourself :("
     assert hello_world(" ") == "hello  "
+
 
 def test_calculate_square():
     assert calculate_square(2) == 4
